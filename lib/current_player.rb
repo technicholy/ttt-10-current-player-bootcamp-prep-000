0eea8d463bd63012710fd4970b040f9b9e557d86
Clayton Pierce <bamer78@gmail.com>
2020-08-24 18:40:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   count = 0
-  board.each {|move| move == ("X"||"O") ? count += 1}
+  board.each {|move| move == ("X"||"O") ? count += 1 : count}
   return count
 end
 def current_player(board)
